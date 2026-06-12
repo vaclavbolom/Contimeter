54b18882c9fb7cc0e05fab56f21f943169f73a85
vaclavbolom <bolom@centrum.cz>
2026-06-12 13:04:01 +0100

    add logging messages import timezone

--- DataPipeline/subscriber.py
+++ DataPipeline/subscriber.py
@@ -2,7 +2,7 @@ import paho.mqtt.client as mqtt
 import time
 import psycopg2
 import json
-from datetime import datetime
+from datetime import datetime, timezone
 
 # List of topics to subscribe to. Add or remove topics as needed.
 TOPICS = [
@@ -19,7 +19,7 @@ STORE_MEASUREMENT_QUERY = """
 def on_connect(client, userdata, flags, return_code, properties):
     try:
         if return_code == 0:
-            print("connected")
+            print(f"connected to {client._host}:{client._port}")
             # subscribe to all configured topics
             for t in TOPICS:
                 client.subscribe(t)
@@ -35,6 +35,7 @@ def on_message(client, userdata, message):
     print("Received message: ", str(message.payload.decode("utf-8")))
     try:
         decoded_message = json.loads(message.payload.decode("utf-8"))
+        print(f"decoded message: {decoded_message}")
         connection = connect_db(parameters)
         send_data(connection, json.dumps(decoded_message))
     except Exception as e:
@@ -47,10 +48,11 @@ def connect_db(db_parameters):
 
 
 def send_data(connection, data):
+    print(f"data to send: {data}")
     data_dict = json.loads(data)
     thingid = data_dict['thingid']
     data_dict.pop('thingid')
-    data_array = [datetime.now(datetime.timezone.utc), thingid, json.dumps(data_dict), json.dumps(data_dict)]
+    data_array = [datetime.now(timezone.utc), thingid, json.dumps(data_dict), json.dumps(data_dict)]
     cursor = connection.cursor()
     cursor.execute(STORE_MEASUREMENT_QUERY, data_array)
     connection.commit()
@@ -60,11 +62,11 @@ def reconnect(client):
     while(not client.is_connected()):
         client.connect(broker_hostname, port) 
         client.loop_start()
-        print("reconnecting client...")
+        print(f"reconnecting client:  {broker_hostname}:{port}...")
         time.sleep(1)
 
 
-broker_hostname ="mosquitto"
+broker_hostname ="127.0.0.1"
 port = 1883 
 parameters = {
         "host": broker_hostname,
